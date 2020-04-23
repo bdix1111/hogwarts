f71b28b61b9f1bbfdb3635e1860c3c6fef26396a
bdix1111 <brianna_dixon@rocketmail.com>
2020-04-23 13:14:31 -0400

    cleared

---
+++ src/components/PigPen.js
@@ -0,0 +1,15 @@
+import React   from "react";
+
+class PigPen extends React.Component {
+  
+  render() {
+
+    return (
+      <div>
+        <h1>Oink Oink</h1>
+      </div>
+    );
+  }
+}
+
+export default PigPen;
